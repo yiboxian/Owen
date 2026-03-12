ee8b9e994b9987fd74af8b08f408861c5ea6ccfd
Suzihan <1450234612@qq.com>
2026-03-12 21:29:10 +0800

    电机/ADC/电源代码初步完成

--- project/user/main.c
+++ project/user/main.c
@@ -35,6 +35,43 @@
 #include "zf_common_headfile.h"
 #include "PD_ENC_SDI.h"
 #include "var.h"
+
+#define CHANNEL_NUMBER          (3)
+#define ADC_CHANNEL1            ( ADC2_CH5_P05 )
+#define ADC_CHANNEL2            ( ADC2_CH6_P06 )
+#define ADC_CHANNEL3            ( ADC2_CH7_P07 )
+
+#define MAX_DUTY            ( 50 )                                               // 最大 MAX_DUTY% 占空比
+
+#define DIR_1               ( IO_P50 ) 
+#define PWM_1               ( PWMD_CH2_P51 )
+                              
+#define DIR_2               ( IO_P52 ) 
+#define PWM_2               ( PWMD_CH4_P53 )
+                              
+#define DIR_3               ( IO_P75 )
+#define PWM_3               ( PWMB_CH1_P74 )
+                              
+#define DIR_4               ( IO_P77 )
+#define PWM_4               ( PWMB_CH3_P76 )
+                              
+#define LED                 ( IO_P52 )
+int8 duty = 0;
+int8 dir = 1;
+
+// #define ADC_CHANNEL1            ( ADC2_CH0_P00 )
+// #define ADC_CHANNEL2            ( ADC2_CH1_P01 )
+// #define ADC_CHANNEL3            ( ADC2_CH2_P02 )
+
+uint8 channel_index = 0;
+adc_channel_enum channel_list[CHANNEL_NUMBER] =
+{
+    ADC_CHANNEL1, 
+    ADC_CHANNEL2, 
+    ADC_CHANNEL3, 
+};
+
+
 void pit_hanlder (void)
 {
     imu660rb_get_acc();                                                         // 获取 IMU660RB 的加速度测量数值
@@ -48,16 +85,100 @@ void main(void)
     clock_init(SYSTEM_CLOCK_96M); // 时钟配置及系统初始化<务必保留>
     debug_init();                 // 调试串口信息初始化
     imu660rb_init();
-	// pit_ms_init(PIT_CH, 100, pit_handler);
-    // 此处编写用户代码 例如外设初始化代码等
+    // // 此处编写用户代码 例如外设初始化代码等
     tim0_irq_handler = pit_hanlder;
     pit_ms_init(TIM0_PIT, 5,pit_hanlder);
+
+    adc_init(ADC_CHANNEL1, ADC_12BIT);          // 初始化对应 ADC 通道为对应精度
+    adc_init(ADC_CHANNEL2, ADC_12BIT);          // 初始化对应 ADC 通道为对应精度
+    adc_init(ADC_CHANNEL3, ADC_12BIT);          // 初始化对应 ADC 通道为对应精度
+
+    
+    gpio_init(LED, GPO, GPIO_HIGH, GPO_PUSH_PULL);     // GPIO 初始化为输出 默认上拉输出高
+
+    // // 此处编写用户代码 例如外设初始化代码等
+    gpio_init(DIR_1, GPO, GPIO_HIGH, GPO_PUSH_PULL);   // GPIO 初始化为输出 默认上拉输出高
+    pwm_init(PWM_1, 17000, 0);                         // PWM 通道初始化频率 17KHz 占空比初始为 0
+
+    gpio_init(DIR_2, GPO, GPIO_HIGH, GPO_PUSH_PULL);   // GPIO 初始化为输出 默认上拉输出高
+    pwm_init(PWM_2, 17000, 0);                         // PWM 通道初始化频率 17KHz 占空比初始为 0
+
+    gpio_init(DIR_3, GPO, GPIO_HIGH, GPO_PUSH_PULL);   // GPIO 初始化为输出 默认上拉输出高
+    pwm_init(PWM_3, 17000, 0);                         // PWM 通道初始化频率 17KHz 占空比初始为 0
+
+    gpio_init(DIR_4, GPO, GPIO_HIGH, GPO_PUSH_PULL);   // GPIO 初始化为输出 默认上拉输出高
+    pwm_init(PWM_4, 17000, 0);                         // PWM 通道初始化频率 17KHz 占空比初始为 0
+    // 此处编写用户代码 例如外设初始化代码等
+
     while(1)
     {
         // 此处编写需要循环执行的代码
-        printf("\r\nIMU660RB gyro data:  x=%5d, y=%5d, z=%5d\r\n", imu660rb_gyro_x, imu660rb_gyro_y, imu660rb_gyro_z);
-		printf("\r\nIMU660RB acc data:  x=%5d, y=%5d, z=%5d\r\n", imu660rb_acc_x, imu660rb_acc_y, imu660rb_acc_z);
-        system_delay_ms(1000);
+        // printf("\r\nIMU660RB gyro data:  x=%5d, y=%5d, z=%5d\r\n", imu660rb_gyro_x, imu660rb_gyro_y, imu660rb_gyro_z);
+		// printf("\r\nIMU660RB acc data:  x=%5d, y=%5d, z=%5d\r\n", imu660rb_acc_x, imu660rb_acc_y, imu660rb_acc_z);
+        // system_delay_ms(1000);
+
+        for(channel_index = 0; channel_index < CHANNEL_NUMBER; channel_index ++)
+        {
+            printf(
+                "ADC channel %d convert data is %d.\r\n",
+                channel_index + 1,
+                adc_convert(channel_list[channel_index]));                      // 循环输出单次转换结果
+        }
+        system_delay_ms(500);
+
+        for(channel_index = 0; channel_index < CHANNEL_NUMBER; channel_index ++)
+        {
+            printf(
+                "ADC channel %d mean filter convert data is %d.\r\n",
+                channel_index + 1,
+                adc_mean_filter_convert(channel_list[channel_index], 10));      // 循环输出 10 次均值滤波转换结果
+        }
+        system_delay_ms(500);
+
+        gpio_toggle_level(LED);  
+        //此处编写需要循环执行的代码
+        if(duty >= 0)                                                           // 正转
+        {
+            gpio_set_level(DIR_1, GPIO_HIGH);                                   // DIR输出高电平
+            pwm_set_duty(PWM_1, duty * (PWM_DUTY_MAX / 100));                   // 计算占空比
+
+            gpio_set_level(DIR_2, GPIO_HIGH);                                   // DIR输出高电平
+            pwm_set_duty(PWM_2, duty * (PWM_DUTY_MAX / 100));                   // 计算占空比
+
+            gpio_set_level(DIR_3, GPIO_HIGH);                                   // DIR输出高电平
+            pwm_set_duty(PWM_3, duty * (PWM_DUTY_MAX / 100));                   // 计算占空比
+
+            gpio_set_level(DIR_4, GPIO_HIGH);                                   // DIR输出高电平
+            pwm_set_duty(PWM_4, duty * (PWM_DUTY_MAX / 100));                   // 计算占空比
+        }
+        else                                                                    // 反转
+        {
+            gpio_set_level(DIR_1, GPIO_LOW);                                    // DIR输出低电平
+            pwm_set_duty(PWM_1, (-duty) * (PWM_DUTY_MAX / 100));                // 计算占空比
+
+            gpio_set_level(DIR_2, GPIO_LOW);                                    // DIR输出低电平
+            pwm_set_duty(PWM_2, (-duty) * (PWM_DUTY_MAX / 100));                // 计算占空比
+
+            gpio_set_level(DIR_3, GPIO_LOW);                                    // DIR输出低电平
+            pwm_set_duty(PWM_3, (-duty) * (PWM_DUTY_MAX / 100));                // 计算占空比
+
+            gpio_set_level(DIR_4, GPIO_LOW);                                    // DIR输出低电平
+            pwm_set_duty(PWM_4, (-duty) * (PWM_DUTY_MAX / 100));                // 计算占空比
+        }
+        if(dir)                                                                 // 根据方向判断计数方向 本例程仅作参考
+        {
+            duty ++;                                                            // 正向计数
+            if(duty >= MAX_DUTY)                                                // 达到最大值
+            dir = 0;                                                            // 变更计数方向
+        }
+        else
+        {
+            duty --;                                                            // 反向计数
+            if(duty <= -MAX_DUTY)                                               // 达到最小值
+            dir = 1;                                                            // 变更计数方向
+        }
+        system_delay_ms(50);
+
         // 此处编写需要循环执行的代码
     }
 }
